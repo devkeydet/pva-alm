10b365a59937a33d869993faa06d07a381e05dfc
Marc Schweigert <devkeydet@users.noreply.github.com>
2021-11-19 10:03:58 -0500

    wip #140

---
+++ .github/actions/import-configuration-migration-data-if-exists/Compress-Data-Files.ps1
@@ -0,0 +1,43 @@
+function Compress-Data-Files ($configPath, $tempConfigurationMigrationDataFolder, $environment) {
+    $commonFolderName = "CommonConfigurationMigrationData"
+    $commonPath = $configPath + $commonFolderName    
+
+    New-Item -Path $tempConfigurationMigrationDataFolder -ItemType Directory
+
+    if (Test-Path $commonPath) {
+        $destinationPath = $tempConfigurationMigrationDataFolder + $commonFolderName + '.zip'
+        $compress = @{
+            Path             = $commonPath + '\*.*'
+            CompressionLevel = 'Fastest'
+            DestinationPath  = $destinationPath
+        }
+        Compress-Archive @compress
+
+        echo "::set-output name=common_zip_path::$destinationPath"
+    }
+    else {
+        echo "No $commonFolderName folder found."
+    }
+
+    if ($environment -notin "pr", "uat", "prod") {
+        $environment = "dev"
+    }
+
+    $environmentFolderName = "EnvironmentConfigrationMigrationData"
+    $environmentPath = $configPath + $environmentFolderName + "/$environment"
+
+    if (Test-Path $environmentPath) {
+        $destinationPath = $tempConfigurationMigrationDataFolder + $environmentFolderName + '.zip'
+        $compress = @{
+            Path             = $environmentPath + '\*.*'
+            CompressionLevel = 'Fastest'
+            DestinationPath  = $destinationPath
+        }
+        Compress-Archive @compress
+
+        echo "::set-output name=environment_zip_path::$destinationPath"
+    }
+    else {
+        echo "No $environmentFolderName folder found."
+    }
+}

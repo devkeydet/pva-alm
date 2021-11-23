831632cb682e9d9b88b2c2ab23f8b337347ae5d1
Marc Schweigert <devkeydet@users.noreply.github.com>
2021-11-23 07:05:29 -0500

    fixes #140

---
+++ .github/workflows/scripts/pwsh/Start-Build-Deploy-Solution.ps1
@@ -0,0 +1,35 @@
+function Start-Build-Deploy-Solution ($files, $githubRef, $prHeadRef, $githubSha, $prHeadSha, $solutionVersionMajorMinor, $environment) {
+    $filesArray = $files -split ','
+
+    $solutionDirectoriesArray = [System.Collections.ArrayList]::new()
+
+    foreach ($file in $filesArray) {         
+        if ($file.StartsWith("src/") -And $file.Contains("SolutionPackage")) {
+            $solutionDirectory = "{0}/{1}/{2}" -f $file.Split('/')
+            $solutionDirectoriesArray.Add($solutionDirectory)
+        }        
+    }
+
+    $uniqueSolutionDirectories = $solutionDirectoriesArray | Sort-Object | Get-Unique
+
+    $solutionNameArray = @()
+
+    foreach ($dir in $uniqueSolutionDirectories) {            
+        $solutionName = $dir.Replace("src/", "").Replace("/SolutionPackage", "")
+        $solutionNameArray = $solutionNameArray + $solutionName
+
+        if ($githubRef.Contains("pull")) {
+            $ref = $prHeadRef
+        }
+        else {
+            $ref = $githubRef
+        }
+
+        gh workflow run build-deploy-solution --ref $ref -f ref=$githubRef -f solution_name=$solutionName -f solution_version_major_minor=$solutionVersionMajorMinor -f environment=$environment -f sha=$prHeadSha
+            
+        echo "pipeline queued for $solutionName"           
+    }
+
+    $csv = $solutionNameArray -join ","
+    echo "::set-output name=solution_names::$csv"
+}

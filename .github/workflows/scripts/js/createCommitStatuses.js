af957c6f9fd5f748d3f29a19fa5a5183e238e061
devkeydet <devkeydet@gmail.com>
2021-11-23 09:39:54 -0500

    resolve conflicts

---
+++ .github/workflows/scripts/js/createCommitStatuses.js
@@ -0,0 +1,19 @@
+module.exports = async ({ github, context, solutionNamesString }) => {
+    console.log(solutionNamesString)
+
+    let solutionNamesArray = solutionNamesString.split(",")
+
+    await solutionNamesArray.forEach(createCommitStatus)
+
+    async function createCommitStatus(solutionName) {
+        let contextToUse = "build-deploy-" + solutionName
+
+        await github.rest.repos.createCommitStatus({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            sha: context.payload.pull_request.head.sha,
+            context: contextToUse,
+            state: "pending"
+        })
+    }
+}
